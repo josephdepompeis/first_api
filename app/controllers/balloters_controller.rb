dbe35b36342afcdaecc44538f046c8db6dbe3d47
Joseph DePompeis <josephdepompeis@gmail.com>
2016-02-25 01:02:25 -0500

    update functionality added

--- app/controllers/balloters_controller.rb
+++ app/controllers/balloters_controller.rb
@@ -11,7 +11,7 @@ class BallotersController < ApplicationController
   def show
     if params["token"]
       token = params["token"]
-      render json:Balloter.where(token: token)
+      render json:Balloter.where(token: token).first
     else
       render json: "Token is either missing or invalid"
     end
@@ -21,6 +21,15 @@ class BallotersController < ApplicationController
   def update
     if params["token"]
       token = params["token"]
-
+      name = params["name"]
+      party = params["party"]
+      updated_balloter = Balloter.where(token: token).first
+      updated_balloter.name = name unless name == nil
+      updated_balloter.party = party unless party == nil
+      updated_balloter.save
+      render json:Balloter.where(token: token).first
+    else
+      render json: "Token is either missing or invalid"
+    end
   end
 end
